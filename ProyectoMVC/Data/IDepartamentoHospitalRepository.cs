83796277f1dbcb2e621893c302d896c271880865
Juan Manuel <lisbethaguirref10@gmail.com>
2025-10-15 11:14:57 -0500

    Primer avance

---
+++ ProyectoMVC/Data/IDepartamentoHospitalRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    interface IDepartamentoHospitalRepository
+    {
+        Task<IEnumerable<DepartamentoHospital>> GetAllAsync();
+        Task<DepartamentoHospital> GetByIdAsync(int id);
+        Task AddAsync(DepartamentoHospital departamento);
+        Task UpdateAsync(DepartamentoHospital departamento);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ ProyectoMVC/Models/DepartamentoHospital.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    class DepartamentoHospital
+    {
+        [Required(ErrorMessage = "El ID del departamento es obligatorio")]
+        public int nIdDepartamento { get; set; }
+
+        [Required(ErrorMessage = "El nombre del departamento es obligatorio")]
+        [StringLength(100, ErrorMessage = "El nombre no puede tener más de 100 caracteres")]
+        public string cNombre { get; set; }
+
+        [Required(ErrorMessage = "La descripción es obligatoria")]
+        [StringLength(200, ErrorMessage = "La descripción no puede tener más de 200 caracteres")]
+        public string cDescripcion { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "La cantidad de personal debe ser cero o mayor")]
+        public int nCantidadPersonal { get; set; }
+
+        [Required(ErrorMessage = "La ubicación es obligatoria")]
+        [StringLength(100, ErrorMessage = "La ubicación no puede tener más de 100 caracteres")]
+        public string cUbicacion { get; set; }
+    }
+}

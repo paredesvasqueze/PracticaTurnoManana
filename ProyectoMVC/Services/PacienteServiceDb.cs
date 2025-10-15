b7db4fa651fd962280ff664049e77e371e5ee4d7
PC2013 <velasquezgils@iestptrujillo.net>
2025-10-15 11:18:59 -0500

    primer commit

--- ProyectoMVC/Services/PacienteServiceDb.cs
+++ ProyectoMVC/Services/PacienteServiceDb.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace Services
 {
-    class PacienteServiceDb
+    public interface IPacienteService
     {
+        Task<IEnumerable<Paciente>> GetAllAsync();
+        Task<Paciente> GetByIdAsync(int id);
+        Task AddAsync(Paciente paciente);
+        Task UpdateAsync(Paciente paciente);
+        Task DeleteAsync(int id);
     }
 }
--- ProyectoMVC/Services/ProductoServiceDb.cs
+++ ProyectoMVC/Services/ProductoServiceDb.cs
@@ -1,27 +1,16 @@
-﻿using Data;
-using Data;
-using Models;
+﻿using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Services
 {
-    public class PacienteServiceDb : IPacienteService
+    public interface IProductoService
     {
-        private readonly IPacienteRepository _repository;
-
-        public PacienteServiceDb(IPacienteRepository repository)
-        {
-            _repository = repository;
-        }
-
-        public Task<IEnumerable<Paciente>> GetAllAsync() => _repository.GetAllAsync();
-        public Task<Paciente> GetByIdAsync(int id) => _repository.GetByIdAsync(id);
-        public Task AddAsync(Paciente paciente) => _repository.AddAsync(paciente);
-        public Task UpdateAsync(Paciente paciente) => _repository.UpdateAsync(paciente);
-        public Task DeleteAsync(int id) => _repository.DeleteAsync(id);
+        Task<IEnumerable<Producto>> GetAllAsync(); Task<Producto> GetByIdAsync(int id);
+        Task AddAsync(Producto producto);
+        Task UpdateAsync(Producto producto);
+        Task DeleteAsync(int id);
     }
 }
